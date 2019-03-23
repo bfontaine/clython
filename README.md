94c097af854b52a326509b352645163b0b06f8d4
Baptiste Fontaine <b@ptistefontaine.fr>
2019-03-23 13:52:56 +0100

    basic doc; jy<->clj conversion

--- README.md
+++ README.md
@@ -1,22 +1,37 @@
-# clython
+# Clython
 
-A Clojure library designed to ... well, that part is up to you.
+**Clython** is Clojure wrapper around [Jython][].
+
+[Jython]: https://www.jython.org/
 
 ## Usage
 
-FIXME
+```clojure
+(ns my.ns
+  (:require [clython.core :as cly]))
+
+(def re-findall (cly/python-import "re" "findall"))
+
+(re-findall "\\w+" "hello from clojure")
+; => ("hello" "from" "clojure")
+
+;; equivalent of `$ python -m SimpleHTTPServer 8000`
+(def http (cly/python-import "SimpleHTTPServer" "test"))
+(http)
+; => Serving HTTP on 0.0.0.0 port 8000 ...
+```
 
 ## License
 
-Copyright © 2019 FIXME
+Copyright © 2019 Baptiste Fontaine
 
-This program and the accompanying materials are made available under the
-terms of the Eclipse Public License 2.0 which is available at
+This program and the accompanying materials are made available under the terms
+of the Eclipse Public License 2.0 which is available at
 http://www.eclipse.org/legal/epl-2.0.
 
 This Source Code may also be made available under the following Secondary
 Licenses when the conditions for such availability set forth in the Eclipse
-Public License, v. 2.0 are satisfied: GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or (at your
+Public License, v. 2.0 are satisfied: GNU General Public License as published
+by the Free Software Foundation, either version 2 of the License, or (at your
 option) any later version, with the GNU Classpath Exception which is available
 at https://www.gnu.org/software/classpath/license.html.
--- src/clython/core.clj
+++ src/clython/core.clj
@@ -1,13 +1,30 @@
 (ns clython.core
   (:require [clojure.string :as str])
   (:import ;[org.python.util PythonInterpreter]
-           [org.python.core PyObject PyNone PyBoolean PyLong PyFloat
-                            PyDictionary PyUnicode
+           [org.python.core
+                            PyArray
+                            PyBoolean
+                            PyBytecode
+                            PyDictionary
+                            PyFloat
+                            PyFrame
+                            PyFunction
+                            PyLong
+                            PyNone
+                            PyObject
+                            PyString
+                            PyUnicode
 
-                            imp PyBytecode PyFrame
+                            imp
                             ]
-         ; [clojure.lang Keyword PersistentArrayMap PersistentHashMap]
+           [clojure.lang Keyword PersistentArrayMap PersistentHashMap
+                         IFn]
+           [java.util HashMap]
            ))
+
+;;
+;; Internal Utilities
+;; ==================
 
 (def ^:private empty-objects (into-array PyObject nil))
 (def ^:private empty-strings (into-array String nil))
@@ -20,29 +37,105 @@
   base-frame
   (PyFrame. base-code (PyDictionary.)))
 
+;;
+;; Conversions
+;; ===========
+;;
+;; Clojure -> Jython
+;; -----------------
+
+(defmulti clj->jy type)
+
+;(defmethod clj->jy nil [_] py-none)
+
+(def ^:private py-true (PyBoolean. true))
+(def ^:private py-false (PyBoolean. false))
+
+(defmethod clj->jy Boolean [x]
+  (if x
+    py-true
+    py-false))
+
+(defmethod clj->jy String [s] (PyUnicode. ^String s))
+(defmethod clj->jy Integer [i] (PyLong. ^Long (long i)))
+(defmethod clj->jy Long [l] (PyLong. ^Long l))
+(defmethod clj->jy Float [f] (PyFloat. ^Float f))
+(defmethod clj->jy Double [d] (PyFloat. ^Double d))
+(defmethod clj->jy Character [c] (PyUnicode. (str c)))
+(defmethod clj->jy Keyword [k] (PyUnicode. (name k)))
+
+(defn- map->jy
+  [m]
+  (let [hm (HashMap. (count m))]
+    (doseq [[k v] m]
+      (.put hm (clj->jy k) (clj->jy v)))
+    (PyDictionary. hm)))
+
+(defmethod clj->jy PersistentHashMap [m] (map->jy m))
+(defmethod clj->jy PersistentArrayMap [m] (map->jy m))
+
+;;
+;; Jython -> Clojure
+;; -----------------
+
+(defmulti jy->clj type)
+
+(defmethod jy->clj PyNone [_] nil)
+(defmethod jy->clj PyUnicode [s] (.asString s))
+(defmethod jy->clj PyString [s] (.asString s))
+(defmethod jy->clj PyBoolean [b] (= py-true b))
+(defmethod jy->clj PyLong [l] (.asLong l))
+(defmethod jy->clj PyFloat [f] (.asDouble f))
+(defmethod jy->clj PyDictionary [d] (into {} d))
+(defmethod jy->clj PyArray [a] (into [] (.asIterable a)))
+
+(defmethod jy->clj PyFunction
+  [f]
+  (reify IFn
+    (invoke [_]
+      (.__call__ f))
+    (invoke [_ a]
+      (.__call__ f (clj->jy a)))
+    (invoke [_ a b]
+      (.__call__ f (clj->jy a) (clj->jy b)))
+    (invoke [_ a b c]
+      (.__call__ f (clj->jy a) (clj->jy b) (clj->jy c)))
+    (invoke [_ a b c d]
+      (.__call__ f (clj->jy a) (clj->jy b) (clj->jy c) (clj->jy d)))
+    (invoke [_ a b c d e & rest*]
+      (.__call__ f (into-array PyObject (concat [a b c d e] rest*))))))
+
+; test
+(defmethod jy->clj :default [x] x)
+
+;;
+;; Modules Import
+;; ==============
+
 (defn python-import
   ([module]
    (. imp importOne module base-frame))
   ([module names]
-   (let [one (string? names)
-         names (if one [names] names)]
-     (cond-> (. imp importFrom module (into-array String names) base-frame 0)
-       one first))))
+   (let [one? (string? names)
+         names (if one? [names] names)]
+     (cond->> (. imp importFrom module (into-array String names) base-frame 0)
+       true (map jy->clj)
+       one? first))))
 
-(defn- clj->py-name
+(defn- sym->py-name
   [name*]
-  (-> name* str (str/replace #"-" "_")))
+  (-> name* name (str/replace #"-" "_")))
 
 (defn- split-names-aliases
   [names]
   (if (map? names)
-    [(mapv clj->py-name (keys names)) (vec (vals names))]
-    [(mapv clj->py-name names) names]))
+    [(mapv sym->py-name (keys names)) (vec (vals names))]
+    [(mapv sym->py-name names) names]))
 
 (defmacro let-import
   "(let-import [pygments [highlight]
                 pygments.lexer [get-lexer-by-name guess-lexer]
-                pygments.formatter [get-formatter-by-name]
+                pygments.formatters [get-formatter-by-name]
                 pygments.util {ClassNotFound class-not-found}]
      ...)"
   [bindings & body]
@@ -50,7 +143,7 @@
          (zero? (mod (count bindings) 2))]}
   (let [let-bindings (vec
                        (mapcat (fn [[python-module names]]
-                                 (let [module-name (str python-module)
+                                 (let [module-name (name python-module)
                                        [py-names
                                         aliases]   (split-names-aliases names)]
                                    `[~aliases (python-import ~module-name (into-array String ~py-names))]))
